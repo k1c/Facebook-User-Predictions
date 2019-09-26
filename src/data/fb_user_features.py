8e74cb5137fe3ac4416eb5afda43dd3980e6eb60
Aditya Joshi <1adityajoshi@gmail.com>
2019-09-25 21:42:14 -0400

    Add user statuses as part of the features

--- src/data/fb_user_features.py
+++ src/data/fb_user_features.py
@@ -2,9 +2,10 @@ from typing import List
 
 
 class FBUserFeatures:
-    def __init__(self, user_id: str, likes: List[int]):
+    def __init__(self, user_id: str, likes: List[int], statuses: List[str]):
         self.user_id = user_id
         self.likes = likes
+        self.statuses = statuses
         self.image = self._load_image(user_id)
 
     # TODO: Change the return type to the actual thing. The `int` is just a placeholder.
